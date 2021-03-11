856ae54201cf1914160ead1d6c2c8fcf6c1dbae6
apolas <tr.liamg@gmail.com>
2021-03-11 22:51:28 +0300

    16-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = 'Apolas';
+  product1: any = {
+    productId: 1,
+    productName: 'bardak',
+    categoryId: 2,
+    unitPrice: 5,
+  };
+  product2: any = {
+    productId: 1,
+    productName: 'masa',
+    categoryId: 2,
+    unitPrice: 5,
+  };
+  product3: any = {
+    productId: 1,
+    productName: 'sandalye',
+    categoryId: 2,
+    unitPrice: 5,
+  };
+  product4: any = {
+    productId: 1,
+    productName: 'çay',
+    categoryId: 2,
+    unitPrice: 5,
+  };
+  product5: any = {
+    productId: 1,
+    productName: 'demlik',
+    categoryId: 2,
+    unitPrice: 5,
+  };
+  products = [
+    this.product1,
+    this.product2,
+    this.product3,
+    this.product4,
+    this.product5,
+  ];
 }
