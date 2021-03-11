e2e1f0488f9a3da8675711485f64fc366e0c5a06
apolas <tr.liamg@gmail.com>
2021-03-11 23:42:25 +0300

    16-son

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,41 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'northwind';
   user: string = 'Apolas';
-  product1: any = {
-    productId: 1,
-    productName: 'bardak',
-    categoryId: 2,
-    unitPrice: 5,
-  };
-  product2: any = {
-    productId: 1,
-    productName: 'masa',
-    categoryId: 2,
-    unitPrice: 5,
-  };
-  product3: any = {
-    productId: 1,
-    productName: 'sandalye',
-    categoryId: 2,
-    unitPrice: 5,
-  };
-  product4: any = {
-    productId: 1,
-    productName: 'çay',
-    categoryId: 2,
-    unitPrice: 5,
-  };
-  product5: any = {
-    productId: 1,
-    productName: 'demlik',
-    categoryId: 2,
-    unitPrice: 5,
-  };
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
+
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  product1: any = {
+    productId: 1,
+    productName: 'bardak',
+    categoryId: 2,
+    unitPrice: 5,
+  };
+  product2: any = {
+    productId: 1,
+    productName: 'masa',
+    categoryId: 3,
+    unitPrice: 4,
+  };
+  product3: any = {
+    productId: 1,
+    productName: 'sandalye',
+    categoryId: 2,
+    unitPrice: 8,
+  };
+  product4: any = {
+    productId: 1,
+    productName: 'çay',
+    categoryId: 1,
+    unitPrice: 57,
+  };
+  product5: any = {
+    productId: 1,
+    productName: 'demlik',
+    categoryId: 2,
+    unitPrice: 5,
+  };
+  products = [
+    this.product1,
+    this.product2,
+    this.product3,
+    this.product4,
+    this.product5,
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
